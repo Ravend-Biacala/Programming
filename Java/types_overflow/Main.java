9ee96d19bc5f9889e472363b7850b56202a967ec
ravend-bicala <draven.calabia2@mail.dcu.ie>
2020-03-17 18:53:20 +0000

    casting and overflow

---
+++ Java/types_overflow/Main.java
@@ -0,0 +1,41 @@
+package Java.types_overflow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum = " + myMinIntValue);
+        System.out.println("Integer Maximum = " + myMaxIntValue);
+        System.out.println("Busted Max value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min value = " + (myMinIntValue - 1));
+        int underTest = 2_147_483_647;
+        System.out.println(underTest);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte M in value = " + (myMinByteValue));
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Min value = " + (myMinShortValue));
+        System.out.println("Short Min value = " + (myMaxShortValue));
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Min value = " + (myMinLongValue));
+        System.out.println("Long Min value = " + (myMaxLongValue));
+        long bitLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bitLongLiteralValue);
+
+        int myTotal = (myMinIntValue /2);
+        byte myNewByteValue = (byte)(myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue /2);
+
+    }
+}
